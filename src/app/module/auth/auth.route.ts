0c9b82bcc3a43994ce19a5f08bce01cf85876819
mdmhrz <razufreelance@gmail.com>
2026-02-21 23:05:48 +0600

    merged with dev

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -3,6 +3,7 @@ import { authController } from "./auth.controller";
 
 const router = Router();
 
+
 router.post("/register", authController.registerPatient);
 router.post("/login", authController.loginUser);
 
