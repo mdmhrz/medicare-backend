57f90e943e378b13cc13b605a1a5407e90984c06
mdmhrz <razufreelance@gmail.com>
2026-03-12 02:31:48 +0600

    Stats module added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -10,6 +10,8 @@ import { AppointmentRoutes } from "../module/appointment/appointment.route";
 import { PatientRoutes } from "../module/patient/patient.route";
 import { PrescriptionRoutes } from "../module/prescription/prescription.route";
 import { ReviewRoutes } from "../module/review/review.route";
+import { StatsRoutes } from "../module/stats/stats.route";
+import { PaymentRoutes } from "../module/payment/payment.route";
 
 const router = Router();
 
@@ -24,6 +26,8 @@ router.use('/doctor-schedules', DoctorScheduleRoutes);
 router.use('/appointments', AppointmentRoutes);
 router.use('/prescription', PrescriptionRoutes);
 router.use('review', ReviewRoutes)
+router.use('/stats', StatsRoutes)
+router.use('/payments', PaymentRoutes)
 
 
 export const IndexRoutes = router;
