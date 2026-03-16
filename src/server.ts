5ea9882c051c38fcff785c1e70505e8e37b68e46
mdmhrz <razufreelance@gmail.com>
2026-03-17 04:16:04 +0600

    part-7 complete

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,49 @@
 
+import { Server } from "http";
 import { app } from "./app";
 import { envVars } from "./app/config/env";
 import { seedSuperAdmin } from "./app/utils/seed";
 
-
+let server: Server;
 
 const bootStrap = async () => {
     try {
         await seedSuperAdmin();
-        app.listen(envVars.PORT, () => {
+        server = app.listen(envVars.PORT, () => {
             console.log(`Server is running on http://localhost:${envVars.PORT}`);
         });
     } catch (err) {
         console.error("Failed to start the server:", err);
     }
 }
+
+
+//uncaught exception handler
+process.on('uncaughtException', (error) => {
+    console.log("Uncaught Exception Detected... Shutting down server", error);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+
+    process.exit(1);
+})
+
+
+//unhandled rejection handler
+
+process.on("unhandledRejection", (error) => {
+    console.log("Unhandled Rejection Detected... Shutting down server", error);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        })
+    }
+
+    process.exit(1);
+})
 
 bootStrap();
