336bd9025ef7aeae629c341303677d5afa1dd511
mdmhrz <razufreelance@gmail.com>
2026-03-17 04:21:28 +0600

    part-7 complete

--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,39 @@ const bootStrap = async () => {
 }
 
 
+// SIGTERM signal handler
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal received. Shutting down server...");
+
+    if (server) {
+        server.close(() => {
+            console.log("Server closed gracefully.");
+            process.exit(1);
+        });
+    }
+
+    process.exit(1);
+
+});
+
+
+// SIGINT signal handler
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal received. Shutting down server...");
+
+    if (server) {
+        server.close(() => {
+            console.log("Server closed gracefully.");
+            process.exit(1);
+        });
+
+    }
+
+    process.exit(1);
+});
+
+
 //uncaught exception handler
 process.on('uncaughtException', (error) => {
     console.log("Uncaught Exception Detected... Shutting down server", error);
@@ -29,7 +62,7 @@ process.on('uncaughtException', (error) => {
     }
 
     process.exit(1);
-})
+});
 
 
 //unhandled rejection handler
@@ -44,6 +77,6 @@ process.on("unhandledRejection", (error) => {
     }
 
     process.exit(1);
-})
+});
 
 bootStrap();
